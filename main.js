db2801304451249ec50699569aee6f2d3662ed95
Enrico Costanza <ec@ecs.soton.ac.uk>
2021-07-20 13:48:35 +0100

    changed js module to use old style

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-import { generateTable } from 'main.js';
+//import { generateTable } from 'table.js';
+
+d3.select(window).on("load", function () {
+
+    console.log('on load');
+    
+    d3.select('#main-button')
+        .on('click', function() {
+            //generateTable();
+            console.log('on click');
+            table_lib.generate_table();
+        });
 
 
-d3.select('#main-button')
-    .on('click', () => {
-        generateTable();
-    });
+});
--- table.js
+++ table.js
@@ -1,3 +1,7 @@
-const generateTable = function () {
-    console.log('generate table');
+const table_lib = (function () {
+    const generateTable = function () {
+        console.log('generate table');
+    }
+    return {'generate_table': generateTable};
 }
+)();
